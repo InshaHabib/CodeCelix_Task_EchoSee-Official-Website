4f32f1a07de7f982885096e6f69cccc854af2227
inshahabib939@gmail.com <inshahabib939@gmail.com>
2026-02-06 21:13:47 +0500

    Updated

--- src/pages/HowItWorksPage.jsx
+++
@@ -1,9 +0,0 @@
-import HowItWorks from "../components/howItWorks/HowItWorks";
-
-export default function HowItWorksPage() {
-  return (
-    <div style={{ background: "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)" }}>
-      <HowItWorks />
-    </div>
-  );
-}
